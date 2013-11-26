ff211338ca969eb18b9fa818c51e2edc5cd00a1d
rgettman <rgettman@455ae3d0-e4df-446e-a8c6-7283b4083675>
2013-11-26 22:14:38 +0000

    Use HSQLDB for JDBC-related tests, instead of mocking up own classes.  Modified site's 0.6.0 release date.

--- jett-core/src/test/java/net/sf/jett/test/JDBCExecutorTest.java
+++ jett-core/src/test/java/net/sf/jett/test/JDBCExecutorTest.java
@@ -1,20 +1,24 @@
 package net.sf.jett.test;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.sql.Connection;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import net.sf.jett.jdbc.JDBCExecutor;
-import net.sf.jett.test.jdbc.MockConnection;
 
 /**
  * This JUnit Test class tests the <code>JDBCExecutor</code> and
@@ -25,6 +29,60 @@ import net.sf.jett.test.jdbc.MockConnection;
  */
 public class JDBCExecutorTest extends TestCase
 {
+   private static Connection theConnection;
+
+   /**
+    * Login to the HSQL DB prior to running any tests.
+    * @throws ClassNotFoundException If the HSQL DB driver was not found.
+    * @throws SQLException If there is an error connecting to the database.
+    */
+   @BeforeClass
+   public static void setUpOnce() throws ClassNotFoundException, SQLException
+   {
+      Class.forName("org.hsqldb.jdbcDriver");
+      theConnection = DriverManager.getConnection("jdbc:hsqldb:file:jett-db", "sa", "");
+
+      try
+      {
+         update("DROP TABLE employee");
+      }
+      catch (SQLException ignored) {}
+      update("CREATE TABLE employee (emp_id INTEGER, first_name VARCHAR(30), last_name VARCHAR(30), " +
+          "salary INTEGER, title VARCHAR(30), manager VARCHAR(60), catch_phrase VARCHAR(100), is_a_manager VARCHAR(1))");
+      update("INSERT INTO employee VALUES (1, 'Robert', 'Stack', 1000, 'Data Structures Programmer', '(none)'       , 'Push, Pop!'                       , 'Y')");
+      update("INSERT INTO employee VALUES (2, 'Suzie',  'Queue',  900, 'Data Structures Programmer', 'Stack, Robert', 'Enqueue, Dequeue!'                , 'N')");
+      update("INSERT INTO employee VALUES (3, 'Elmer',  'Fudd',   800, 'Cartoon Character'         , 'Bunny, Bugs'  , 'I''m hunting wabbits!  Huh-uh-uh!', 'N')");
+      update("INSERT INTO employee VALUES (4, 'Bugs',   'Bunny', 1500, 'Cartoon Character'         , '(none)'       , 'Ah, what''s up Doc?'              , 'Y')");
+   }
+
+   /**
+    * Runs the given SQL Statement via a <code>Statement</code> and a call to
+    * <code>executeUpdate</code>.
+    * @param sql The SQL Statement.
+    * @throws SQLException If there is a problem executing the query.
+    */
+   private static void update(String sql) throws SQLException
+   {
+      Statement st = theConnection.createStatement();
+      st.executeUpdate(sql);
+      st.close();
+   }
+
+   /**
+    * Close the connection to the HSQL DB after all tests are run.
+    * @throws SQLException If there is a problem closing the connection.
+    */
+   @AfterClass
+   public static void cleanUpOnce() throws SQLException
+   {
+      if (theConnection != null)
+      {
+         Statement st = theConnection.createStatement();
+         st.execute("SHUTDOWN");
+         theConnection.close();
+      }
+   }
+
    /**
     * Tests the .xls template spreadsheet.
     * @throws java.io.IOException If an I/O error occurs.
@@ -64,17 +122,14 @@ public class JDBCExecutorTest extends TestCase
     */
    protected void check(Workbook workbook)
    {
-      // To test the JDBCExecutor, I didn't bundle a DB along with drivers.
-      // I minimally implemented some JDBC interfaces in net.sf.jett.test.jdbc
-      // here in test/java.
       Sheet query = workbook.getSheetAt(0);
       assertEquals("Robert", TestUtility.getStringCellValue(query, 1, 0));
       assertEquals("Queue", TestUtility.getStringCellValue(query, 2, 1));
       assertEquals(800, TestUtility.getNumericCellValue(query, 3, 2), Double.MIN_VALUE);
       assertEquals("Cartoon Character", TestUtility.getStringCellValue(query, 4, 3));
-      assertTrue(TestUtility.isCellBlank(query, 1, 4));
+      assertEquals("(none)", TestUtility.getStringCellValue(query, 1, 4));
       assertEquals("I'm hunting wabbits!  Huh-uh-uh!", TestUtility.getStringCellValue(query, 3, 5));
-      assertTrue(TestUtility.getBooleanCellValue(query, 4, 6));
+      assertEquals("Y", TestUtility.getStringCellValue(query, 4, 6));
 
       Sheet prepared = workbook.getSheetAt(1);
       assertEquals("Cartoon Character", TestUtility.getStringCellValue(prepared, 0, 0));
@@ -104,8 +159,7 @@ public class JDBCExecutorTest extends TestCase
    protected Map<String, Object> getBeansMap()
    {
       Map<String, Object> beans = new HashMap<String, Object>();
-      Connection conn = new MockConnection();
-      JDBCExecutor jdbc = new JDBCExecutor(conn);
+      JDBCExecutor jdbc = new JDBCExecutor(theConnection);
       beans.put("jdbc", jdbc);
 
       List<String> titleSearches = Arrays.asList("Cartoon Character", "Data Structures Programmer", "Nonexistent Title");
