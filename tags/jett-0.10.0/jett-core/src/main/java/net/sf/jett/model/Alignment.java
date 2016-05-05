571ab042ca7b9f70198158fd690b72c7d0b6d0c2
rgettman <rgettman@455ae3d0-e4df-446e-a8c6-7283b4083675>
2016-05-05 20:53:50 +0000

    [maven-release-plugin]  copy for tag jett-0.10.0

---
+++ tags/jett-0.10.0/jett-core/src/main/java/net/sf/jett/model/Alignment.java
@@ -0,0 +1,56 @@
+package net.sf.jett.model;
+
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+
+/**
+ * <p><code>Alignments</code> represent the built-in horizontal alignment names
+ * that correspond with Excel's horizontal alignment scheme.  These are used in
+ * conjunction with the alignment property in the style tag.  Legal values are
+ * the names of the enumeration objects, without underscores, case insensitive,
+ * e.g. "center" == "Center" == "CENTER".</p>
+ *
+ * @author Randy Gettman
+ * @since 0.4.0
+ * @see net.sf.jett.tag.StyleTag
+ * @see net.sf.jett.parser.StyleParser#PROPERTY_ALIGNMENT
+ */
+public enum Alignment
+{
+   CENTER         ((short) HorizontalAlignment.CENTER.ordinal()),
+   CENTERSELECTION((short) HorizontalAlignment.CENTER_SELECTION.ordinal()),
+   DISTRIBUTED    ((short) HorizontalAlignment.DISTRIBUTED.ordinal()),
+   FILL           ((short) HorizontalAlignment.FILL.ordinal()),
+   GENERAL        ((short) HorizontalAlignment.GENERAL.ordinal()),
+   JUSTIFY        ((short) HorizontalAlignment.JUSTIFY.ordinal()),
+   LEFT           ((short) HorizontalAlignment.LEFT.ordinal()),
+   RIGHT          ((short) HorizontalAlignment.RIGHT.ordinal());
+
+   private short myIndex;
+
+   /**
+    * Constructs an <code>Alignment</code>.
+    * @param index The index.
+    */
+   Alignment(short index)
+   {
+      myIndex = index;
+   }
+
+   /**
+    * Returns the index.
+    * @return The index.
+    */
+   public short getIndex()
+   {
+      return myIndex;
+   }
+
+   /**
+    * Returns the name, in all lowercase, no underscores or spaces.
+    * @return The name, in all lowercase, no underscores or spaces.
+    */
+   public String toString()
+   {
+      return name().trim().toLowerCase().replace("_", "");
+   }
+}
