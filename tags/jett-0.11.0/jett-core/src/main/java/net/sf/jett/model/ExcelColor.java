c821c05272008e9bf2ee07a049d5e41176c897c2
rgettman <rgettman@455ae3d0-e4df-446e-a8c6-7283b4083675>
2018-05-14 22:48:18 +0000

    [maven-release-plugin] copy for tag jett-0.11.0

---
+++ tags/jett-0.11.0/jett-core/src/main/java/net/sf/jett/model/ExcelColor.java
@@ -0,0 +1,256 @@
+package net.sf.jett.model;
+
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+
+/**
+ * <p><code>Colors</code> represent the built-in color names that correspond
+ * with Excel's indexed color scheme.  These are used in conjunction with
+ * several property names defined for the style tag.  These color names do NOT
+ * necessarily correspond with HTML/CSS standard color names.  Legal values are
+ * the names of the enumeration objects, without underscores, case insensitive,
+ * e.g. "center" == "Center" == "CENTER".</p>
+ *
+ * @author Randy Gettman
+ * @since 0.4.0
+ * @see net.sf.jett.tag.StyleTag
+ * @see net.sf.jett.parser.StyleParser#PROPERTY_BORDER_COLOR
+ * @see net.sf.jett.parser.StyleParser#PROPERTY_BOTTOM_BORDER_COLOR
+ * @see net.sf.jett.parser.StyleParser#PROPERTY_LEFT_BORDER_COLOR
+ * @see net.sf.jett.parser.StyleParser#PROPERTY_RIGHT_BORDER_COLOR
+ * @see net.sf.jett.parser.StyleParser#PROPERTY_TOP_BORDER_COLOR
+ * @see net.sf.jett.parser.StyleParser#PROPERTY_FILL_BACKGROUND_COLOR
+ * @see net.sf.jett.parser.StyleParser#PROPERTY_FILL_FOREGROUND_COLOR
+ * @see net.sf.jett.parser.StyleParser#PROPERTY_FONT_COLOR
+ */
+public enum ExcelColor
+{
+    AQUA               (new HSSFColor.AQUA()                 , IndexedColors.AQUA                 , 51, 204, 204),
+    AUTOMATIC          (new HSSFColor.AUTOMATIC()            , IndexedColors.AUTOMATIC            , 0, 0, 0),
+    BLACK              (new HSSFColor.BLACK()                , IndexedColors.BLACK                , 0, 0, 0),
+    BLUE               (new HSSFColor.BLUE()                 , IndexedColors.BLUE                 , 0, 0, 255),
+    BLUEGREY           (new HSSFColor.BLUE_GREY()            , IndexedColors.BLUE_GREY            , 102, 102, 153),
+    BRIGHTGREEN        (new HSSFColor.BRIGHT_GREEN()         , IndexedColors.BRIGHT_GREEN         , 0, 255, 0),
+    BROWN              (new HSSFColor.BROWN()                , IndexedColors.BROWN                , 153, 51, 0),
+    CORAL              (new HSSFColor.CORAL()                , IndexedColors.CORAL                , 255, 128, 128),
+    CORNFLOWERBLUE     (new HSSFColor.CORNFLOWER_BLUE()      , IndexedColors.CORNFLOWER_BLUE      , 153, 153, 255),
+    DARKBLUE           (new HSSFColor.DARK_BLUE()            , IndexedColors.DARK_BLUE            , 0, 0, 128),
+    DARKGREEN          (new HSSFColor.DARK_GREEN()           , IndexedColors.DARK_GREEN           , 0, 51, 0),
+    DARKRED            (new HSSFColor.DARK_RED()             , IndexedColors.DARK_RED             , 128, 0, 0),
+    DARKTEAL           (new HSSFColor.DARK_TEAL()            , IndexedColors.DARK_TEAL            , 0, 51, 102),
+    DARKYELLOW         (new HSSFColor.DARK_YELLOW()          , IndexedColors.DARK_YELLOW          , 128, 128, 0),
+    GOLD               (new HSSFColor.GOLD()                 , IndexedColors.GOLD                 , 255, 204, 0),
+    GREEN              (new HSSFColor.GREEN()                , IndexedColors.GREEN                , 0, 128, 0),
+    GREY25PERCENT      (new HSSFColor.GREY_25_PERCENT()      , IndexedColors.GREY_25_PERCENT      , 192, 192, 192),
+    GREY40PERCENT      (new HSSFColor.GREY_40_PERCENT()      , IndexedColors.GREY_40_PERCENT      , 150, 150, 150),
+    GREY50PERCENT      (new HSSFColor.GREY_50_PERCENT()      , IndexedColors.GREY_50_PERCENT      , 128, 128, 128),
+    GREY80PERCENT      (new HSSFColor.GREY_80_PERCENT()      , IndexedColors.GREY_80_PERCENT      , 51, 51, 51),
+    INDIGO             (new HSSFColor.INDIGO()               , IndexedColors.INDIGO               , 51, 51, 153),
+    LAVENDER           (new HSSFColor.LAVENDER()             , IndexedColors.LAVENDER             , 204, 153, 255),
+    LEMONCHIFFON       (new HSSFColor.LEMON_CHIFFON()        , IndexedColors.LEMON_CHIFFON        , 255, 255, 204),
+    LIGHTBLUE          (new HSSFColor.LIGHT_BLUE()           , IndexedColors.LIGHT_BLUE           , 51, 102, 255),
+    LIGHTCORNFLOWERBLUE(new HSSFColor.LIGHT_CORNFLOWER_BLUE(), IndexedColors.LIGHT_CORNFLOWER_BLUE, 204, 204, 255),
+    LIGHTGREEN         (new HSSFColor.LIGHT_GREEN()          , IndexedColors.LIGHT_GREEN          , 204, 255, 204),
+    LIGHTORANGE        (new HSSFColor.LIGHT_ORANGE()         , IndexedColors.LIGHT_ORANGE         , 255, 153, 0),
+    LIGHTTURQUOISE     (new HSSFColor.LIGHT_TURQUOISE()      , IndexedColors.LIGHT_TURQUOISE      , 204, 255, 255),
+    LIGHTYELLOW        (new HSSFColor.LIGHT_YELLOW()         , IndexedColors.LIGHT_YELLOW         , 255, 255, 153),
+    LIME               (new HSSFColor.LIME()                 , IndexedColors.LIME                 , 153, 204, 0),
+    MAROON             (new HSSFColor.MAROON()               , IndexedColors.MAROON               , 128, 0, 0),
+    OLIVEGREEN         (new HSSFColor.OLIVE_GREEN()          , IndexedColors.OLIVE_GREEN          , 51, 51, 0),
+    ORANGE             (new HSSFColor.ORANGE()               , IndexedColors.ORANGE               , 255, 102, 0),
+    ORCHID             (new HSSFColor.ORCHID()               , IndexedColors.ORCHID               , 102, 0, 102),
+    PALEBLUE           (new HSSFColor.PALE_BLUE()            , IndexedColors.PALE_BLUE            , 153, 204, 255),
+    PINK               (new HSSFColor.PINK()                 , IndexedColors.PINK                 , 255, 0, 255),
+    PLUM               (new HSSFColor.PLUM()                 , IndexedColors.PLUM                 , 153, 51, 102),
+    RED                (new HSSFColor.RED()                  , IndexedColors.RED                  , 255, 0, 0),
+    ROSE               (new HSSFColor.ROSE()                 , IndexedColors.ROSE                 , 255, 103, 204),
+    ROYALBLUE          (new HSSFColor.ROYAL_BLUE()           , IndexedColors.ROYAL_BLUE           , 0, 102, 204),
+    SEAGREEN           (new HSSFColor.SEA_GREEN()            , IndexedColors.SEA_GREEN            , 51, 153, 102),
+    SKYBLUE            (new HSSFColor.SKY_BLUE()             , IndexedColors.SKY_BLUE             , 0, 204, 255),
+    TAN                (new HSSFColor.TAN()                  , IndexedColors.TAN                  , 255, 204, 153),
+    TEAL               (new HSSFColor.TEAL()                 , IndexedColors.TEAL                 , 0, 128, 128),
+    TURQUOISE          (new HSSFColor.TURQUOISE()            , IndexedColors.TURQUOISE            , 0, 255, 255),
+    VIOLET             (new HSSFColor.VIOLET()               , IndexedColors.VIOLET               , 128, 0, 128),
+    WHITE              (new HSSFColor.WHITE()                , IndexedColors.WHITE                , 255, 255, 255),
+    YELLOW             (new HSSFColor.YELLOW()               , IndexedColors.YELLOW               , 255, 255, 0);
+
+    /**
+     * The "automatic" color in HSSF (.xls).
+     * @since 0.9.1
+     */
+    public static final HSSFColor HSSF_COLOR_AUTOMATIC = new HSSFColor.AUTOMATIC();
+
+    /**
+     * The color index used by comments in XSSF (.xlsx).
+     * @since 0.10.0
+     */
+    public static final short XSSF_COLOR_COMMENT = 81;
+
+    private HSSFColor myHssfColor;
+    private XSSFColor myXssfColor;
+    private IndexedColors myIndexedColor;
+    private int myRed;
+    private int myGreen;
+    private int myBlue;
+
+    private static HSSFColor[] hssfColors;
+
+    static
+    {
+        hssfColors = new HSSFColor[65];
+        for (ExcelColor excelColor : values())
+        {
+            HSSFColor hssfColor = excelColor.getHssfColor();
+            hssfColors[hssfColor.getIndex()] = hssfColor;
+        }
+    }
+
+    /**
+     * Creates a <code>ExcelColor</code>.
+     * @param hssfColor The <code>HSSFColor</code>.
+     * @param indexedColor The <code>IndexedColor</code>.
+     * @param red The red value, 0-255.
+     * @param green The green value, 0-255.
+     * @param blue The blue value, 0-255.
+     */
+    ExcelColor(HSSFColor hssfColor, IndexedColors indexedColor, int red, int green, int blue)
+    {
+        myHssfColor = hssfColor;
+        myXssfColor = new XSSFColor(new byte[] {(byte) red, (byte) green, (byte) blue});
+        myIndexedColor = indexedColor;
+        myRed = red;
+        myGreen = green;
+        myBlue = blue;
+    }
+
+    /**
+     * Return the <code>HSSFColor</code>.
+     * @return The <code>HSSFColor</code>.
+     */
+    public HSSFColor getHssfColor()
+    {
+        return myHssfColor;
+    }
+
+    /**
+     * Return the <code>XSSFColor</code>.
+     * @return The <code>XSSFColor</code>.
+     */
+    public XSSFColor getXssfColor()
+    {
+        return myXssfColor;
+    }
+
+    /**
+     * Returns the index.
+     * @return The index.
+     */
+    public int getIndex()
+    {
+        return myIndexedColor.getIndex();
+    }
+
+    /**
+     * Returns the <code>IndexedColors</code>.
+     * @return The <code>IndexedColors</code>.
+     */
+    public IndexedColors getIndexedColor()
+    {
+        return myIndexedColor;
+    }
+
+    /**
+     * Returns the red value, 0-255.
+     * @return The red value, 0-255.
+     */
+    public int getRed()
+    {
+        return myRed;
+    }
+
+    /**
+     * Returns the green value, 0-255.
+     * @return The green value, 0-255.
+     */
+    public int getGreen()
+    {
+        return myGreen;
+    }
+
+    /**
+     * Returns the blue value, 0-255.
+     * @return The blue value, 0-255.
+     */
+    public int getBlue()
+    {
+        return myBlue;
+    }
+
+    /**
+     * Returns the hex string, in the format "#RRGGBB".
+     * @return The hex string, in the format "#RRGGBB".
+     */
+    public String getHexString()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.append("#");
+
+        String value = Integer.toHexString(myRed);
+        if (value.length() == 1)
+            builder.append("0");
+        builder.append(value);
+
+        value = Integer.toHexString(myGreen);
+        if (value.length() == 1)
+            builder.append("0");
+        builder.append(value);
+
+        value = Integer.toHexString(myBlue);
+        if (value.length() == 1)
+            builder.append("0");
+        builder.append(value);
+
+        return builder.toString();
+    }
+
+    /**
+     * Returns the "distance" of the given RGB triplet from this color, as
+     * defined by the sum of each of the differences for the red, green, and
+     * blue values.
+     * @param red The red value.
+     * @param green The green value.
+     * @param blue The blue value.
+     * @return The sum of each of the differences for the red, green, and blue
+     *    values.
+     */
+    public int distance(int red, int green, int blue)
+    {
+        return Math.abs(red - myRed) + Math.abs(green - myGreen) + Math.abs(blue - myBlue);
+    }
+
+    /**
+     * Returns the color name, in all lowercase, no underscores or spaces.
+     * @return The color name, in all lowercase, no underscores or spaces.
+     */
+    @Override
+    public String toString()
+    {
+        return name().trim().toLowerCase().replace("_", "");
+    }
+
+    /**
+     * Maps a short index color back to an <code>HSSFColor</code>.
+     * @param index A short color index.
+     * @return An <code>HSSFColor</code>.
+     */
+    public static HSSFColor getHssfColorByIndex(short index)
+    {
+        if (index == Font.COLOR_NORMAL || index == XSSF_COLOR_COMMENT)
+        {
+            return HSSF_COLOR_AUTOMATIC;
+        }
+        return hssfColors[index];
+    }
+}
---
+++ tags/jett-0.11.0/readme.txt
@@ -0,0 +1,144 @@
+JETT - Java Excel Template Translator
+----------------
+
+JETT can be found online at http://jett.sourceforge.net.
+
+Description
+----------------
+
+JETT is Java Excel Template Translator.  Give it a Map of "beans" -- mapping
+variable names to your own data objects, specify a pre-existing Excel template
+file, and JETT will create a new Excel spreadsheet, populating your data into
+it.  The data can come from any source.  Mark up your template spreadsheet
+first, with JEXL Expressions (e.g. "${myVariable}") and XML-like Tags (e.g.
+<jt:forEach items="${myList}" var="item">) for control over how JETT translates
+your template spreadsheet and populates your data.  You can even create your
+own Tags to implement custom processing logic.  JETT works with .xls and .xlsx
+Excel files.
+
+Example
+----------------
+
+Here is a quick example of how to use JETT:
+
+- Excel template spreadsheet:
+
++----------------+----------------+
+|${var}          |${var2}!        |
++----------------+----------------+
+
+- Java code to use JETT:
+
+Map<String, Object> beans = new HashMap<String, Object>();
+beans.put("var", "Hello");
+beans.put("var2", "World");
+ExcelTransformer transformer = new ExcelTransformer();
+try
+{
+   transformer.transform("template.xlsx", "result.xlsx", beans);
+}
+catch (IOException e)
+{
+   System.err.println("I/O error occurred: " + e.getMessage());
+}
+catch (InvalidFormatException e)
+{
+   System.err.println("Spreadsheet was in invalid format: " + e.getMessage());
+}
+
+- The resultant Excel spreadsheet:
+
++----------------+----------------+
+|Hello           |World!          |
++----------------+----------------+
+
+Installation
+----------------
+
+To use JETT, you may download the latest distribution from
+http://sourceforge.net/projects/jett/files/.  The only module is "jett-core".
+Place the "jett-core" jar library in your classpath.
+
+See "Dependencies" below for the list of dependencies.
+
+If you are using Maven 2+, then you may place the following dependency in your
+pom.xml.  Since 0.3.0, JETT has been available in the Maven 2 Central
+Repository.
+
+<dependency>
+    <groupId>net.sf.jett</groupId>
+    <artifactId>jett-core</artifactId>
+    <version>0.11.0</version>
+</dependency>
+
+Dependencies
+----------------
+
+JETT can be used with Java 1.7+.  JETT depends on several external libraries:
+
+The following libraries are required, as they are used by JETT:
+- Apache POI 3.14 (http://poi.apache.org/download.html) (or higher)
+   - poi-3.14-20160307.jar
+   - poi-ooxml-3.14-20160307.jar
+   - poi-ooxml-schemas-3.14-20160307.jar
+   - Apache POI, in turn, depends on the following library: XML Beans 2.6.0.
+- XML Beans 2.6.0
+   - xmlbeans-2.6.0.jar (Comes with Apache POI distribution)
+- Apache Commons JEXL 2.1.1 (http://commons.apache.org/jexl/download_jexl.cgi)
+   - commons-jexl-2.1.1.jar
+   - Apache Commons JEXL 2.1.1 in turn depends on Commons Logging 1.1.1.
+- Apache Commons Logging 1.2 (http://commons.apache.org/logging/download_logging.cgi)
+   - commons-logging-1.2.jar
+- SourceForge's jAgg 0.9.0 (http://sourceforge.net/projects/jagg/files/) (or higher)
+   - jagg-core-0.9.0.jar
+   - Full disclosure: I built jAgg also.
+- JUnit 4.8.2 (for testing only)
+   - junit-4.8.2.jar
+- HSQLDB 1.8.0.10 (for testing only)
+   - hsqldb-1.8.0.10.jar
+- Apache Log4j2 (https://logging.apache.org/log4j/2.x/download.html) (or higher)
+   - log4j-api-2.9.0.jar
+   - log4j-core-2.9.0.jar
+   - log4j-jcl-2.9.0.jar (So JEXL's Commons Logging goes through Log4j2)
+
+Build Instructions
+----------------
+
+If you would like to build JETT yourself, do the following:
+1. Get the source code.
+  a. Download the JETT latest distribution from
+     http://sourceforge.net/projects/jett/files/.
+     This contains the source code from the latest release.
+  OR
+  b. Checkout the latest source code from the trunk using Subversion using the
+     Subversion URL http://svn.code.sf.net/p/jett/code-0/trunk.
+
+2. Get Maven 2 or higher from http://maven.apache.org/ and install it.
+
+3. Run Maven to build JETT.
+   mvn clean install
+
+Contacts
+----------------
+
+For issues, bugs, suggestions, and feature requests, please send an email to
+the "jett-users" mailing list: jett-users@lists.sourceforge.net.
+
+Licensing
+----------------
+
+JETT is licensed under the "GNU Lesser General Public License Version 3" at
+http://www.gnu.org/copyleft/lesser.html.
+
+Author
+----------------
+
+My name is Randy Gettman.  For a while I used jXLS for translating Excel
+template spreadsheets using Java.  At the time, jXLS suffered from poor
+performance when supplying lots of data.  I was inspired to create JETT after
+this experience so that I could create spreadsheets from templates quickly, and
+with some features that jXLS didn't have.  By the time I released JETT, jXLS
+had fixed a few performance issues, so they perform similarly.  However, JETT
+has a number of features that jXLS doesn't have (jXLS has a few features that
+JETT doesn't (yet) have.)  The bottom line is that I wanted a jXLS with more
+features, and JETT is the result.
